435159737779ccff701564ec1dae191ec80b6322
Sam Richard <sam@snug.ug>
2016-04-18 07:20:26 -0400

    Change Background color

--- libs/server.js
+++ libs/server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><title>Hello World</title></head><body><h1>Hello World</h1></body></html>');
+  res.send('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><title>Hello World</title><style>body{background:#c0ffee;}</style></head><body><h1>Hello World</h1></body></html>');
 });
 
 
