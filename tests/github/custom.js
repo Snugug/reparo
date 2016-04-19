ee090dfafb74dc6bf549011485d15af17f39615e
Sam Richard <sam@snug.ug>
2016-04-19 07:49:30 -0400

    :art: Move GitHub tests in to folder

--- tests/github/custom.js
+++ tests/github/custom.js
@@ -2,10 +2,12 @@ import test from 'ava';
 
 const keys = [ 'config', 'debug', 'version', '3.0.0', 'constants', 'requestHeaders', 'gists', 'getGistsApi', 'gitdata', 'getGitdataApi', 'issues', 'getIssuesApi', 'authorization', 'getAuthorizationApi', 'orgs', 'getOrgsApi', 'statuses', 'getStatusesApi', 'pullRequests', 'getPullrequestsApi', 'repos', 'getReposApi', 'user', 'getUserApi', 'events', 'getEventsApi', 'releases', 'getReleasesApi', 'search', 'getSearchApi', 'markdown', 'getMarkdownApi', 'gitignore', 'getGitignoreApi', 'misc', 'getMiscApi' ];
 
-test('Default GitHub Auth', t => {
-  const github = require('../libs/github');
+test('Custom GitHub Host', t => {
+  process.env['GHE'] = 'github.company.com';
+
+  const github = require('../../libs/github');
 
   t.deepEqual(Object.keys(github), keys, 'Contains all endpoint functions');
-  t.is(github.config.host, 'api.github.com', 'GitHub public API endpoint');
-  t.is(github.config.pathPrefix, '/', 'GitHub public path prefix');
+  t.is(github.config.host, 'github.company.com', 'Custom GitHub API endpoint');
+  t.is(github.config.pathPrefix, '/api/v3', 'GitHub public path prefix');
 });
