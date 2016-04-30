89733a1888390ac6231b2f2df182f4b4f62ceac9
Sam Richard <sam@snug.ug>
2016-04-30 16:17:08 -0400

    Add GitHub oAuth

--- index.js
+++ index.js
@@ -6,6 +6,10 @@
 const express = require('express');
 const config = require('config');
 const nunjucks = require('nunjucks');
+const path = require('path');
+const flash = require('connect-flash');
+
+const oauth = require('./lib/oauth')();
 
 //////////////////////////////
 // App Variables
@@ -21,6 +25,22 @@ nunjucks.configure(['views', 'templates'], {
   'express': app,
 });
 app.set('view engine', 'html');
+
+app.use(flash());
+app.use((req, res, next) => {
+  const token = req.flash('token');
+  let auth = false;
+
+  if (token.length >= 1) {
+    auth = true;
+    req.flash('token', token)
+  }
+
+  res.locals.auth = auth;
+  next();
+});
+
+//////////////////////////////
 // Routing
 //////////////////////////////
 app.get('/', (req, res) => {
@@ -28,6 +48,24 @@ app.get('/', (req, res) => {
     authenticated: res.locals.auth
   });
 });
+
+// OAuth
+app.get('/login', (req, res) => {
+  return oauth.login(req, res);
+});
+
+app.get('/callback', (req, res) => {
+  return oauth.callback(req, res);
+});
+
+oauth.on('error', function (err, token, res, tokenRes, req) {
+  req.flash('error message', err);
+  res.redirect('/');
+});
+
+oauth.on('token', function (token, res, tokenRes, req) {
+  req.flash('token', token);
+  res.redirect('/');
 });
 
 //////////////////////////////
