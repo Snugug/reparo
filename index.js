f7eddad9a6ac1f72b0fe6b2d98186f9e09fb5c26
Sam Richard <sam@snug.ug>
2016-04-30 09:31:45 -0400

    Add coverage for index.js

--- index.js
+++ index.js
@@ -11,10 +11,20 @@ const config = require('config');
 //////////////////////////////
 const app = express();
 
+//////////////////////////////
+// Routing
+//////////////////////////////
 app.get('/', (req, res) => {
   res.send('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><title>Hello World</title><style>body{background:#c0ffee;}</style></head><body><h1>Hello World</h1></body></html>');
 });
 
+//////////////////////////////
+// Start the server
+//
+// Ignored from code coverage as it's not possible to actually write a test for
+// Starts the actual when this file is called directly
+//////////////////////////////
+/* istanbul ignore if  */
 if (!module.parent) {
   app.listen(config.server.port, () => {
     console.log(`Server starting on ${config.server.host}:${config.server.port}`);
